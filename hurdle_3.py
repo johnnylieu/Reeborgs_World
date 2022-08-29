6cedb6f63155fccf6216df72e70eeb399513036a
Johnny <JohnnyLieu@yahoo.com>
2022-08-29 14:55:48 -0700

    fixed again

--- hurdle_3.py
+++ hurdle_3.py
@@ -2,18 +2,21 @@ def turn_right():
     turn_left()
     turn_left()
     turn_left()
-
-def hurdle(s):
-    for i in range(0, s):
-        move()
-        turn_left()
-        move()
-        turn_right()
-        move()
-        turn_right()
-        move()
-        turn_left()
  
-hurdle(6)
+def avoid_wall():
+        turn_left()
+        move()
+        turn_right()
+        move()
+        turn_right()
+        move()
+        turn_left()
+        
+while not at_goal():
+    if front_is_clear():
+        move()
+    if wall_in_front():
+        avoid_wall()
+ 
 
-#Reeborg's World Sunday Funday w/ Johnny Lieu
+#Reeborg's World Monday Funday w/ Johnny Lie
